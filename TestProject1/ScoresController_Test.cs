e51273bbba4ea606462dbeefc0aa1aa62e75f59d
OFFICE\vagarwal <vagarwal@csod.com>
2023-03-22 00:36:05 +0530

    completed the unit testing

--- TestProject1/ScoresController_Test.cs
+++ TestProject1/ScoresController_Test.cs
@@ -20,6 +20,8 @@ namespace ScoresController_Test
         [InlineData(40,30,20,10)]
         [InlineData(40,00,20,00)]
         [InlineData(00,00,00,00)]
+        [InlineData(00,00,01,00)]
+        [InlineData(100,100,100,100)]
         public void CompleteFlow_Test(int collaborate, int create, int compete, int control)
         {
             //setup
@@ -54,9 +56,33 @@ namespace ScoresController_Test
 
             serviceOutput = scoresController.CheckRulesHandler(requestOfCheckRules);
             var evaluatedScore = (serviceOutput as OkObjectResult).Value as EvaluatedScoreResponse;
+                        
+            //assert
+            Assert.True(processedScore.Data.Scaled.Collaborate.Value % 1 == 0);
+            Assert.True(processedScore.Data.Scaled.Create.Value % 1 == 0);
+            Assert.True(processedScore.Data.Scaled.Compete.Value % 1 == 0);
+            Assert.True(processedScore.Data.Scaled.Control.Value % 1 == 0);
+            Dictionary<Culture,int> scores = new Dictionary<Culture, int> 
+            {
+                { Culture.Collaborate, collaborate },
+                { Culture.Create, create },
+                { Culture.Compete, compete },
+                { Culture.Control, control },
+            };
+            var highestScoreCulture = scores.Where(x => x.Value == scores.Values.Max()).First().Key;
+            Assert.True(processedScore.Data.Ranked.First == highestScoreCulture);
 
-            //Assert
-            //TODO: assert raw,scaled & rank
+            scores = new Dictionary<Culture, int>
+            {
+                { Culture.Collaborate, (int)processedScore.Data.Scaled.Collaborate.Value },
+                { Culture.Create, (int)processedScore.Data.Scaled.Create.Value },
+                { Culture.Compete,  (int)processedScore.Data.Scaled.Compete.Value },
+                { Culture.Control,  (int)processedScore.Data.Scaled.Control.Value },
+            };
+            
+            if(!scores.Values.All(x=>x==0))
+            Assert.True(scores.Where(x=>x.Key == highestScoreCulture).First().Value ==100);
+
             if (collaborate == 0 && create == 0 && compete == 0 && control == 0)
             {
                 Assert.Equal(evaluatedScore.Results[0].Result, EvaluationResult.Applied);
@@ -79,28 +105,25 @@ namespace ScoresController_Test
         }
 
         [Theory]
-        [InlineData(10, 20, 30, 40)]
-        [InlineData(40, 30, 20, 10)]
-        [InlineData(40, 00, 20, 00)]
-        [InlineData(00, 00, 00, 00)]
+        [InlineData(151, 160, 140, 50)]
         public void ProcessScoreHandler_Test(int collaborate, int create, int compete, int control)
         {
             //setup
             var scoring = new Mock<IScoring>();            
             ScaledScore scaledScore = new ScaledScore
             {
-                Collaborate = new CultureScore { Culture = Culture.Collaborate, Value = 0 },
-                Create = new CultureScore { Culture = Culture.Create, Value = 0 },
-                Compete = new CultureScore { Culture = Culture.Compete, Value = 0 },
-                Control = new CultureScore { Culture = Culture.Control, Value = 0 }                
+                Collaborate = new CultureScore { Culture = Culture.Collaborate, Value = 94 },
+                Create = new CultureScore { Culture = Culture.Create, Value = 100 },
+                Compete = new CultureScore { Culture = Culture.Compete, Value = 88 },
+                Control = new CultureScore { Culture = Culture.Control, Value = 31 }                
             };
             //below todo
             RankedCulture rankedCulture= new RankedCulture
             {
-                //First = arr[3].Culture,
-                //Second = arr[2].Culture,
-                //Third = arr[1].Culture,
-                //Fourth = arr[0].Culture,
+                First = scaledScore.Create.Culture,
+                Second = scaledScore.Collaborate.Culture,
+                Third = scaledScore.Compete.Culture,
+                Fourth = scaledScore.Control.Culture
             };
 
             scoring.Setup(x => x.Scale(It.IsAny<RawScore>())).Returns(scaledScore);
@@ -133,8 +156,6 @@ namespace ScoresController_Test
         [InlineData(00, 00, 00, 00)]
         public void CheckRulesHandler_Test(int collaborate, int create, int compete, int control)
         {
-            //completed
-            //setup
             var scoring = new Mock<IScoring>();
             ScoresController scoresController = new ScoresController(scoring.Object);
             IActionResult serviceOutput = null;
